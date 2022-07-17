197a82b3f4f22be355688c1323aa2a2e2e332bf2
cyrus <Cyrus>
2022-07-17 10:11:04 -0500

    Can now split large builds into 2 db entries

--- etheria_newBuildUsher/index.js
+++ etheria_newBuildUsher/index.js
@@ -1,9 +1,9 @@
 const AWS = require('aws-sdk');
 var options = {
-  maxRetries: 1,
-  httpOptions: {
-    timeout: 900000
-  }
+	maxRetries: 1,
+	httpOptions: {
+		timeout: 900000
+	}
 };
 const lambda = new AWS.Lambda(options);
 const dynamoDB = new AWS.DynamoDB.DocumentClient({ region: 'us-east-1' });
@@ -33,149 +33,235 @@ const pako = require('pako');
 //          newBuildUsher is the only one that needs DynamoDB and pako
 
 function isNumeric(str) {
-    if (typeof str != "string")
-        return false; // we only process strings!  
-    return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
-        !isNaN(parseFloat(str)); // ...and ensure strings of whitespace fail
+	if (typeof str != "string")
+		return false; // we only process strings!  
+	return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+		!isNaN(parseFloat(str)); // ...and ensure strings of whitespace fail
 }
 
 exports.handler = async (event) => {
-    console.log("event=" + JSON.stringify(event));
+	console.log("event=" + JSON.stringify(event));
 
-    return new Promise((resolve, reject) => {
+	return new Promise((resolve, reject) => {
 
-        if (!event || Object.keys(event).length === 0) {
-            reject(new Error("event is invalid or missing"));
-            return;
-        }
+		if (!event || Object.keys(event).length === 0) {
+			reject(new Error("event is invalid or missing"));
+			return;
+		}
 
-        if (!event.params) {
-            reject(new Error("event.params is invalid or missing"));
-            return;
-        }
+		if (!event.params) {
+			reject(new Error("event.params is invalid or missing"));
+			return;
+		}
 
-        if (!event.params.querystring) {
-            reject(new Error("event.params.querystring is invalid or missing"));
-            return;
-        }
-        console.log("querystring=" + JSON.stringify(event.params.querystring));
+		if (!event.params.querystring) {
+			reject(new Error("event.params.querystring is invalid or missing"));
+			return;
+		}
+		console.log("querystring=" + JSON.stringify(event.params.querystring));
 
-        if (!event.params.querystring.tileIndex || !isNumeric(event.params.querystring.tileIndex) || (event.params.querystring.tileIndex * 1) > 1088) { // non-existant, not numeric or greater than tile index limit
-            reject(new Error("event.params.querystring.tileIndex is invalid or missing"));
-            return;
-        }
-        console.log("tileIndex=" + JSON.stringify(event.params.querystring.tileIndex));
+		if (!event.params.querystring.tileIndex || !isNumeric(event.params.querystring.tileIndex) || (event.params.querystring.tileIndex * 1) > 1088) { // non-existant, not numeric or greater than tile index limit
+			reject(new Error("event.params.querystring.tileIndex is invalid or missing"));
+			return;
+		}
+		console.log("tileIndex=" + JSON.stringify(event.params.querystring.tileIndex));
 
-        if (!event.params.querystring.blockNumber || !isNumeric(event.params.querystring.blockNumber)) {
-            reject(new Error("event.params.querystring.blockNumber is invalid or missing"));
-            return;
-        }
-        console.log("blockNumber=" + JSON.stringify(event.params.querystring.blockNumber));
+		if (!event.params.querystring.blockNumber || !isNumeric(event.params.querystring.blockNumber)) {
+			reject(new Error("event.params.querystring.blockNumber is invalid or missing"));
+			return;
+		}
+		console.log("blockNumber=" + JSON.stringify(event.params.querystring.blockNumber));
 
-        if (!event.params.querystring.hexString) {
-            reject(new Error("event.params.querystring.hexString is invalid or missing"));
-            return;
-        }
-        console.log("hexString=" + JSON.stringify(event.params.querystring.hexString));
+		if (!event.params.querystring.hexString) {
+			reject(new Error("event.params.querystring.hexString is invalid or missing"));
+			return;
+		}
+		console.log("hexString=" + JSON.stringify(event.params.querystring.hexString));
 
-        if (!
-            (
-                event.params.querystring.version === "0.9" ||
-                event.params.querystring.version === "1.0" ||
-                event.params.querystring.version === "1.1" ||
-                event.params.querystring.version === "1.2"
-            )
-        ) {
-            reject(new Error("Invalid or missing version parameter"));
-            return;
-        }
-        console.log("version=" + event.params.querystring.version);
+		if (!
+			(
+				event.params.querystring.version === "0.9" ||
+				event.params.querystring.version === "1.0" ||
+				event.params.querystring.version === "1.1" ||
+				event.params.querystring.version === "1.2"
+			)
+		) {
+			reject(new Error("Invalid or missing version parameter"));
+			return;
+		}
+		console.log("version=" + event.params.querystring.version);
 
-        lambda.invoke({
-            FunctionName: "arn:aws:lambda:us-east-1:540151370381:function:etheria_hexStringToBuild",
-            Payload: JSON.stringify({
-                "body-json": {},
-                "params": {
-                    "path": {},
-                    "querystring": {
-                        "hexString": event.params.querystring.hexString
-                    }
-                }
-            }) // pass params
-        }, function(err, data) {
-            if (err) {
-                reject(err);
-            }
-            else {
-                //console.log("data back from getMapState=" + JSON.stringify(JSON.parse(data.Payload),null,2)); // successful response
-                var hexShapes = JSON.parse(data.Payload);
+		lambda.invoke({
+			FunctionName: "arn:aws:lambda:us-east-1:540151370381:function:etheria_hexStringToBuild",
+			Payload: JSON.stringify({
+				"body-json": {},
+				"params": {
+					"path": {},
+					"querystring": {
+						"hexString": event.params.querystring.hexString
+					}
+				}
+			}) // pass params
+		}, function(err, data) {
+			if (err) {
+				reject(err);
+			}
+			else {
+				console.log("data back from getMapState and dataPayload.length=" + data.Payload.length); // successful response
+				var hexShapes = JSON.parse(data.Payload);
+				var hexShapesStringified = JSON.stringify(hexShapes); //.replaceAll("\"", "");
+				console.log("hexShapesStringified.length=" + hexShapesStringified.length);
+				var compressed = pako.deflate(hexShapesStringified, { to: 'string' });
+				console.log("typeof compressed=" + typeof compressed);
+				
+				var compressed1 = "";
+				var compressed2 = "";
+				var splitInTwoParts = false;
+				if(compressed.length > 300000)
+				{
+					compressed1 = compressed.slice(0,300000);
+					compressed2 = compressed.slice(300000);
+					splitInTwoParts = true;
+				}
+				else
+					compressed1 = compressed;
+				
+//				
+//				if (hexShapesStringified.length > 3000000) {
+//					compressed1 = pako.deflate(hexShapesStringified.substring(0, 3000000), { to: 'string' });
+//					compressed2 = pako.deflate(hexShapesStringified.substring(3000000), { to: 'string' });
+//					splitInTwoParts = true;
+//				}
+//				else
+//					compressed1 = pako.deflate(hexShapesStringified, { to: 'string' });
 
-                var compressed = pako.deflate(JSON.stringify(hexShapes), { to: 'string' });
-                console.log("compressed.length=" + compressed.length);
-                var params = {
-                    TableName: 'EtheriaBuildsUnified',
-                    Item: {
-                        'tileIndexAndVersion': event.params.querystring.tileIndex + "v" + event.params.querystring.version,
-                        'blockNumber': (event.params.querystring.blockNumber*1),
-                        'build': compressed,
-                        'tileIndex': event.params.querystring.tileIndex,
-                        'version': event.params.querystring.version,
-//                        'hexString': event.params.querystring.hexString // large builds won't fit if we include this full hex string. Sucks but w/e
-                    }
-                };
+				console.log("compressed1.length=" + compressed1.length);
+				console.log("compressed2.length=" + compressed2.length);
 
-                dynamoDB.put(params, function(err, data) {
-                    if (err) {
-                        reject(err);
-                    }
-                    else {
-                        console.log("EtheriaBuildsUnified put success.");
+				var params = {
+					TableName: 'EtheriaBuildsUnified',
+					Item: {
+						'tileIndexAndVersion': event.params.querystring.tileIndex + "v" + event.params.querystring.version,
+						'blockNumber': (event.params.querystring.blockNumber * 1),
+						'build': compressed1,
+						'tileIndex': event.params.querystring.tileIndex,
+						'version': event.params.querystring.version,
+						'hexString': event.params.querystring.hexString
+					}
+				};
 
-                        // update buildIndices globalVar. This is the running list of the indices with actual builds on them
-                        // so we don't have to grab every build for every tile everytime the explorer or builder is loaded
-                        var params = {
-                            TableName: "EtheriaGlobalVars",
-                            Key: {
-                                "name": "buildIndicesV" + event.params.querystring.version
-                            }
-                        };
+				dynamoDB.put(params, function(err, data) {
+					if (err) {
+						reject(err);
+					}
+					else {
+						console.log("EtheriaBuildsUnified(1) put success.");
 
-                        dynamoDB.get(params, function(err, globalVarEntry) {
-                            if (err) {
-                                console.log("Error", err);
-                                reject(err);
-                            }
-                            else {
-                                console.log("success getting buildIndices globalVar", JSON.stringify(globalVarEntry));
-                                var buildIndices = JSON.parse(globalVarEntry.Item.value);
-                                var buildIndicesSet = new Set(buildIndices); // convert to set to eliminate dupes
-                                console.log("adding " + event.params.querystring.tileIndex + " to buildIndices globalVar...");
-                                buildIndicesSet.add(event.params.querystring.tileIndex * 1);
-                                buildIndices = [...buildIndicesSet]; // back to array
-                                console.log("done. Array is now " + JSON.stringify(buildIndices));
-                                var params2 = {
-                                    TableName: "EtheriaGlobalVars",
-                                    Item: {
-                                        "name": "buildIndicesV" + event.params.querystring.version,
-                                        "value": JSON.stringify(buildIndices)
-                                    }
-                                };
-                                dynamoDB.put(params2, function(err, data) { // update
-                                    if (err) {
-                                        console.log(" error updating buildIndices globalVar", err);
-                                        reject();
-                                    }
-                                    else {
-                                        console.log(" success putting buildIndices globalVar");
-                                        resolve();
-                                    }
-                                });
-                            }
-                        });
+						if (splitInTwoParts === true) {
+							var params2 = {
+								TableName: 'EtheriaBuildsUnified',
+								Item: {
+									'tileIndexAndVersion': event.params.querystring.tileIndex + "v" + event.params.querystring.version + "_2",
+									'blockNumber': (event.params.querystring.blockNumber * 1),
+									'build': compressed2,
+									'tileIndex': event.params.querystring.tileIndex,
+									'version': event.params.querystring.version,
+									'hexString': event.params.querystring.hexString
+								}
+							};
 
-                    }
-                });
-            }
-        });
-    });
+							dynamoDB.put(params2, function(err, data) {
+								if (err) {
+									reject(err);
+								}
+								else {
+									console.log("EtheriaBuildsUnified(2) put success.");
+									var params = {
+										TableName: "EtheriaGlobalVars",
+										Key: {
+											"name": "buildIndicesV" + event.params.querystring.version
+										}
+									};
+
+									dynamoDB.get(params, function(err, globalVarEntry) {
+										if (err) {
+											console.log("Error", err);
+											reject(err);
+										}
+										else {
+											console.log("success getting buildIndices globalVar", JSON.stringify(globalVarEntry));
+											var buildIndices = JSON.parse(globalVarEntry.Item.value);
+											var buildIndicesSet = new Set(buildIndices); // convert to set to eliminate dupes
+											console.log("adding " + event.params.querystring.tileIndex + " to buildIndices globalVar...");
+											buildIndicesSet.add(event.params.querystring.tileIndex * 1);
+											buildIndices = [...buildIndicesSet]; // back to array
+											console.log("done. Array is now " + JSON.stringify(buildIndices));
+											var params2 = {
+												TableName: "EtheriaGlobalVars",
+												Item: {
+													"name": "buildIndicesV" + event.params.querystring.version,
+													"value": JSON.stringify(buildIndices)
+												}
+											};
+											dynamoDB.put(params2, function(err, data) { // update
+												if (err) {
+													console.log(" error updating buildIndices globalVar", err);
+													reject();
+												}
+												else {
+													console.log(" success putting buildIndices globalVar");
+													resolve();
+												}
+											});
+										}
+									});
+								}
+							});
+						}
+						else { // mmmm that sweet code duplication
+							var params = {
+								TableName: "EtheriaGlobalVars",
+								Key: {
+									"name": "buildIndicesV" + event.params.querystring.version
+								}
+							};
+
+							dynamoDB.get(params, function(err, globalVarEntry) {
+								if (err) {
+									console.log("Error", err);
+									reject(err);
+								}
+								else {
+									console.log("success getting buildIndices globalVar", JSON.stringify(globalVarEntry));
+									var buildIndices = JSON.parse(globalVarEntry.Item.value);
+									var buildIndicesSet = new Set(buildIndices); // convert to set to eliminate dupes
+									console.log("adding " + event.params.querystring.tileIndex + " to buildIndices globalVar...");
+									buildIndicesSet.add(event.params.querystring.tileIndex * 1);
+									buildIndices = [...buildIndicesSet]; // back to array
+									console.log("done. Array is now " + JSON.stringify(buildIndices));
+									var params2 = {
+										TableName: "EtheriaGlobalVars",
+										Item: {
+											"name": "buildIndicesV" + event.params.querystring.version,
+											"value": JSON.stringify(buildIndices)
+										}
+									};
+									dynamoDB.put(params2, function(err, data) { // update
+										if (err) {
+											console.log(" error updating buildIndices globalVar", err);
+											reject();
+										}
+										else {
+											console.log(" success putting buildIndices globalVar");
+											resolve();
+										}
+									});
+								}
+							});
+						}
+					}
+				});
+			}
+		});
+	});
 };
